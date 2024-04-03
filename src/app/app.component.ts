282863be3703e5100169a861c4f36d7c22138d4e
Benjamin Holzapfel <benjamin.holzapfel@gs-computerservice.de>
2024-04-03 09:26:33 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Breakpoints, BreakpointObserver} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit() {
+    this.breakpointObserver.observe(Breakpoints.HandsetLandscape)
+      .subscribe(result => {
+
+        if (result.matches) {
+          console.log("screens matches HandsetLandscape");
+        }
+
+  });
+    
+  }
 }
